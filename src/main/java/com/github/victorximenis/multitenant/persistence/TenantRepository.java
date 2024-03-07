f80daba50b7651092045c15aa4297ca44755c756
Victor Ximenis <victorximenis@gmail.com>
2024-03-07 00:54:28 -0300

    Adicionando @Repository

--- src/main/java/com/github/victorximenis/multitenant/persistence/TenantRepository.java
+++ src/main/java/com/github/victorximenis/multitenant/persistence/TenantRepository.java
@@ -2,10 +2,12 @@ package com.github.victorximenis.multitenant.persistence;
 
 import com.github.victorximenis.multitenant.model.Tenant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TenantRepository extends JpaRepository<Tenant, UUID> {
     Optional<Tenant> findTenantByTenantId(String id);
 }
